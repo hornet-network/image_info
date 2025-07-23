1b6222dd953f463a082461b12d3a5fba354eb6e0
Matthew Hirst <hirst.mat@gmail.com>
2025-07-23 13:38:09 +0300

    Add timeout and max redirect options to configuration. (#11)

--- lib/image_info/configuration.rb
+++ lib/image_info/configuration.rb
@@ -1,10 +1,13 @@
 module ImageInfo
   class Configuration
-    attr_accessor :max_concurrency, :max_image_size
+    attr_accessor :max_concurrency, :max_image_size, :redirect_limit, :http_open_timeout, :http_read_timeout
 
     def initialize
       @max_concurrency = 20
       @max_image_size = -1
+      @redirect_limit = 50
+      @http_open_timeout = nil
+      @http_read_timeout = nil
     end
   end
 end
--- lib/image_info/processor.rb
+++ lib/image_info/processor.rb
@@ -8,13 +8,13 @@ module ImageInfo
   class Processor
     attr_reader :images, :options
 
-    def initialize(urls, options = { max_concurrency: ::ImageInfo.config.max_concurrency })
+    def initialize(urls, options = { max_concurrency: ::ImageInfo.config.max_concurrency, redirect_limit: ::ImageInfo.config.redirect_limit, http_open_timeout: ::ImageInfo.config.http_open_timeout, http_read_timeout: ::ImageInfo.config.http_read_timeout})
       @images  = Array(urls).map { |uri| ::ImageInfo::Image.new(uri) }.keep_if(&:valid?)
       @options = options
     end
 
     def process
-      images.each { |image| hydra.queue(::ImageInfo::RequestHandler.new(image).build) }
+      images.each { |image| hydra.queue(::ImageInfo::RequestHandler.new(image, options: options).build) }
       hydra.run
 
       images
